d3406d78fcf8ff8e229db23ba96d5146c078c6cc
abdelaziz <abdelaziz.59140@gmail.com>
2019-12-13 15:56:52 +0100

    add exercices

--- Exo3/main.py
+++ Exo3/main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     c = Customer("charles", "chalgoumi", 39, "vélo", 100)
     customer = ("Idendités complétes du client:\n nom: {}\n prénom: {}\n age: {} ans\n panier: {}\n montant: {}€".format(c.nom, c.prénom, c.age, c.panier, c.montant))
     print(customer)
-    print("-----------------------------------------------------")
+    print("--------------------------------------------------------")
     e = Employee("boules", "praliné", 24, "cadre")
     employe = ("Idendités complétes de l'employé:\n nom: {}\n prénom: {}\n age: {} ans\n statut: {}".format(e.nom, e.prénom, e.age, e.statut))
     print(employe)
